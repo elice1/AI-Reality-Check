cd31c228b16c7de90bc9430a08b54c7b42eabb35
elice1 <elicedejesus@gmail.com>
2026-05-12 20:49:55 +0800

    Update factcheck.js

--- api/factcheck.js
+++ api/factcheck.js
@@ -1,4 +1,4 @@
-const GEMINI_API = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent";
+const GEMINI_API = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
